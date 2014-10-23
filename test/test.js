40b4b65bdf084468863dcfe51f6c67e437fd089c
cedric <delcroix.cedric62@gmail.com>
2014-10-23 14:56:35 +0200

    premier commit test1

--- test/test.js
+++ test/test.js
@@ -4,3 +4,6 @@ var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
 };
+PalettoTestCase.prototype.test1 = function () {
+    assert(mafonction() == true);
+};
