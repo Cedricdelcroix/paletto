8cf439b3c8f72073e39d7f2b496d88fb6479a8e0
cedric <delcroix.cedric62@gmail.com>
2014-10-23 15:35:28 +0200

    fonction init plateau test juxtaposition

--- src/Engine.js
+++ src/Engine.js
@@ -1,11 +1,49 @@
 'use strict';
 
-var Engine = function () {
+var Paletto = function () {
 
 // private attributes and methods
-
+var pligne = new Array(6);
+    for(var ligne=0; ligne < pligne.length; ligne++){
+        pligne[ligne] = new Array(6);
+    }
 // public methods
-    this.mafonction = function(){
-        return true;
+    this.initplateau = function(){
+        pligne[0][0] = "noir";
+        pligne[1][0] = "jaune";
+        pligne[2][0] = "bleu";
+        pligne[3][0] = "rouge";
+        pligne[4][0] = "blanc";
+        pligne[5][0] = "jaune";
+        pligne[0][1] = "vert";
+        pligne[1][1] = "blanc";
+        pligne[2][1] = "jaune";
+        pligne[3][1] = "noir";
+        pligne[4][1] = "vert";
+        pligne[5][1] = "bleu";
+        pligne[0][2] = "blanc";
+        pligne[1][2] = "vert";
+        pligne[2][2] = "bleu";
+        pligne[3][2] = "rouge";
+        pligne[4][2] = "jaune";
+        pligne[5][2] = "noir";
+        pligne[0][3] = "bleu";
+        pligne[1][3] = "rouge";
+        pligne[2][3] = "blanc";
+        pligne[3][3] = "vert";
+        pligne[4][3] = "noir";
+        pligne[5][3] = "rouge";
+        pligne[0][4] = "rouge";
+        pligne[1][4] = "jaune";
+        pligne[2][4] = "noir";
+        pligne[3][4] = "bleu";
+        pligne[4][4] = "jaune";
+        pligne[5][4] = "vert";
+        pligne[0][5] = "blanc";
+        pligne[1][5] = "bleu";
+        pligne[2][5] = "rouge";
+        pligne[3][5] = "blanc";
+        pligne[4][5] = "vert";
+        pligne[5][5] = "noir";
     }
 };
--- test/test.js
+++ test/test.js
@@ -4,7 +4,9 @@ var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
 };
-PalettoTestCase.prototype.test1 = function () {
-    var eng = new Engine();
-     assert(eng.mafonction() == true);
+PalettoTestCase.prototype.testjuxtapo = function () {
+    var plateau = new Plateau();
+    var ligne =0;
+    var colonne = 0;
+    assert(plateau.getjuxtapo[ligne][colonne] = true)
 };
