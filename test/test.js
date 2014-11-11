cce3e3b3745929906298a62690815fcc03814fc4
cedric <delcroix.cedric62@gmail.com>
2014-11-11 17:32:14 +0100

    test story 3

--- test/test.js
+++ test/test.js
@@ -14,4 +14,12 @@ PalettoTestCase.prototype.testchoseplayer1 = function () {
     plateau.initplateau();
     var choseplayer1 = "jaune"
     assert(plateau.checkcoin(choseplayer1)==true);
-}
+};
+PalettoTestCase.prototype.testStory3 = function () {
+    var plateau = new Paletto();
+    plateau.initplateau();
+    plateau.removecase();
+    assert(plateau.getplateau(5,0)== "empty");
+    assert(plateau.getplayercountbillebycolor(1,"jaune"));
+    assert(plateau.countnbbille()== 35);
+};
