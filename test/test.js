263f3a07473bf156d487f6e21a0893f9693c66dd
cedric <delcroix.cedric62@gmail.com>
2014-11-11 16:52:36 +0100

    test choose player 1 jaune

--- test/test.js
+++ test/test.js
@@ -9,3 +9,9 @@ PalettoTestCase.prototype.testjuxtapo = function () {
     plateau.initplateau();
     assert(plateau.getjuxtapo() == true)
 };
+PalettoTestCase.prototype.testchoseplayer1 = function () {
+    var plateau = new Paletto();
+    plateau.initplateau();
+    var choseplayer1 = "jaune"
+    assert(plateau.checkcoin(choseplayer1)==true);
+}
